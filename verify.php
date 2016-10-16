7cdeb7128b4e59dfd6369c4e5f2b7058966ed0b8
l3osslunla <Suphanut_boss@outlook.com>
2016-10-16 13:41:55 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,3 +1,6 @@
+$proxy = 'http://fixie:aP4gg2vqMBeibUf@velodrome.usefixie.com:80';
+$proxyauth = 'suphanut_boss@outlook.com:071037Boss';
+
 <?php
 $access_token = 'bVDx+7g9W7OUMvxxJ2+IUjxTvKSv4nlPZPRyxkDcINZ/eC89Uf8wnX5yZaJms5vad5skYVsIneSv3xUN9U324abOMfMlbdOkp7FQijSQeOmjNZLdgieRj6cPwVllgml/u9UhWnhxj1TIV0qlHVT9kQdB04t89/1O/w1cDnyilFU=';
 
@@ -9,6 +12,8 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
