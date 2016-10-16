a7b3bd6e6d6ebd20bfba0ef3b5f32940eece7705
l3osslunla <Suphanut_boss@outlook.com>
2016-10-16 13:44:15 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 $proxy = 'http://fixie:aP4gg2vqMBeibUf@velodrome.usefixie.com:80';
-$proxyauth = 'suphanut_boss@outlook.com:071037Boss';
+$proxyauth = 'fixie:aP4gg2vqMBeibUf';
 
 <?php
 $access_token = 'bVDx+7g9W7OUMvxxJ2+IUjxTvKSv4nlPZPRyxkDcINZ/eC89Uf8wnX5yZaJms5vad5skYVsIneSv3xUN9U324abOMfMlbdOkp7FQijSQeOmjNZLdgieRj6cPwVllgml/u9UhWnhxj1TIV0qlHVT9kQdB04t89/1O/w1cDnyilFU=';
